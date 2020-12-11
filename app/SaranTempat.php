0fdf42eacbf61f3fa7e7c97cbcd62318065ec2db
Handi Wijaya <hwijaya91@gmail.com>
2020-12-11 09:38:39 +0700

    up saran tempat

--- app/SaranTempat.php
+++ app/SaranTempat.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class SaranTempat extends Model
 {
-    //
+    protected $table = 'sarans_tempats';
+    protected $fillable = [
+        'orangs_id',
+        'kotas_id',
+        'kategoris_id',
+        'saran_tempat',
+        'alamat',
+        'gmaps',
+        'deskripsi',
+    ];
+
+    public static function ins($orang,$kota,$tempat,$alamat,$deskripsi,$kategori,$gmaps=''){
+        $ins = new SaranTempat([
+            'orangs_id'     => $orang,
+            'kotas_id'      => $kota,
+            'kategoris_id'  => $kategori,
+            'saran_tempat'  => $tempat,
+            'alamat'        => $alamat,
+            'gmaps'         => $gmaps,
+            'deskripsi'     => $deskripsi
+        ]);
+        $cek = $ins->save();
+        if($cek){
+            return 'yes';
+        } else return 'no';
+    }
 }
